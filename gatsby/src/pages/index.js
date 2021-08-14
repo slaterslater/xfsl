18ddfd7062424c073f1d520122dab0973f3c9dfd
slaterslater <slaterslater@gmail.com>
2021-08-14 19:57:26 -0400

    adds dayjs to refactor index.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,32 +1,22 @@
 import { graphql } from "gatsby"
 import React, { useEffect, useState } from "react"
+import dayjs from 'dayjs'
 import SEO from "../components/SEO"
 
 const IndexPage = ({ data }) => {
   const [thisWeek, setThisWeek] = useState([])
 
   useEffect(() => {
-    const now = new Date()
-    now.setTime(now.getTime() - now.getTimezoneOffset() * 6000) // adjusts for ISO
-    const today = now.getDate()
-
-    // finds two dates seven days apart in YYYY-MM-DD format
-    const [gt_date, lte_date] = [today, today + 7].map(date => {
-      now.setDate(date - process.env.GATSBY_OFFSET_DAYS)
-      return now.toISOString().split("T")[0]
-    })
-
-    const isInDateRange = game => game.date > gt_date && game.date <= lte_date
-
-    const games = data.games.nodes.filter(game => isInDateRange(game))
+    const day = num => dayjs().day(num).format('YYYY-MM-DD')
+    const isThisWeek = game => game.date > day(0) && game.date <= day(7)
+    const games = data.games.nodes.filter(game => isThisWeek(game))
     setThisWeek(games)
   }, [])
 
   return (
     <>
       <SEO title="Home" />
-      <p>index</p>
-      <p>there {thisWeek.length} games</p>
+      <h2>{dayjs(thisWeek[0]?.date).format('MMMM D')}</h2>
       <ul>
         {thisWeek.map(game => (
           <li key={game.id}>
