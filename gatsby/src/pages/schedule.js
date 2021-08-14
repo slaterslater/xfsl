3e17d7a97bf8b1411f0c718079180e8bc326882e
slaterslater <slaterslater@gmail.com>
2021-08-14 15:37:58 -0400

    adds season state as array of week objects

--- gatsby/src/pages/schedule.js
+++ gatsby/src/pages/schedule.js
@@ -1,16 +1,50 @@
-import React, { useContext } from "react"
-import { LeagueContext } from "../components/LeagueContext"
+import { graphql } from "gatsby"
+import React, { useEffect, useState } from "react"
 import SEO from "../components/SEO"
 
-const SchedulePage = () => {
-  const { weeks } = useContext(LeagueContext)
+const SchedulePage = ({data}) => {
+    const [season, setSeason] = useState({})
 
+   useEffect(()=>{
+    const weeks = data.games.nodes.reduce((season, game) => {
+      let week = season[season.length -1]
+      if (week?.date != game.date) {
+        week = {
+          week : game.week,
+          date : game.date,
+          games : [game]
+        }
+        season.push(week)
+      } else {
+        week.games.push(game)
+      }
+      return season
+    }, [])
+    setSeason(weeks)
+  },[])
+  
   return (
     <>
       <SEO title="Schedule" />
-      <p>Schedule of {weeks}</p>
+      <p>Schedule of {season.length} weeks</p>
     </>
   )
 }
 
 export default SchedulePage
+
+export const query = graphql`
+query {
+  games: allSanityGame(sort: {fields: [date, time], order: [ASC, ASC]}) {
+    nodes {
+      away
+      home
+      winner
+      id
+      time
+      week
+      date
+    }
+  }
+}
+`
