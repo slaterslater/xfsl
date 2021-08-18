3bbe2702ed8688e149a66bc7b86c3cbe5edd8c8c
slaterslater <slaterslater@gmail.com>
2021-08-17 23:15:24 -0400

    updates og:image url

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -22,15 +22,15 @@ export default function SEO({ children, location, description, title, image }) {
         {site.siteMetadata.title}
       </title>
       {/* Fav icons  */}
-      <link rel="alternate icon" href="favicon.ico" />
-      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       {/* Meta tags  */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charset="UTF-8" />
       <meta name="description" content={site.siteMetadata.description} />
       {/* Open Graph  */}
       <meta property="og:url" content={site.siteMetadata.siteUrl} key="ogurl" />
-      <meta property="og:image" content={"favicon.svg"} key="ogimage" />
+      <meta property="og:image" content={"/favicon.svg"} key="ogimage" />
       <meta
         property="og:title"
         content={site.siteMetadata.title}
